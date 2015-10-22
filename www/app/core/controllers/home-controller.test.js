7edc7061c0696b3d6052178ed1415240a0f3df2d
Wendy Sanarwanto <saintc0d3r@gmail.com>
2015-10-22 12:44:15 +0800

    Added a line to load app.core module before all tests begin in home-controller.test.js

--- www/app/core/controllers/home-controller.test.js
+++ www/app/core/controllers/home-controller.test.js
@@ -1,7 +1,11 @@
 describe("Home controller's behaviours or functions", function(){
+	// Mock controller's $scope & Heroes service before each tests are executed	
+	// Load the module we want to test
+	beforeEach(module('app.core'));
+	
 	it("should be able to return list of pickable heroes", function(){
 		// Arrange
-					
+							
 		// Act
 		
 		// Assert
